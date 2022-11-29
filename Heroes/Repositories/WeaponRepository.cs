10dfe34b995fb74ad5225fdacd104b4aa72479fe
Gergana Georgieva <gerganagergana1@gmail.com>
2022-11-29 19:55:56 +0200

    C# OOP Retake Exam - 18 April 2022

---
+++ Heroes/Repositories/WeaponRepository.cs
@@ -0,0 +1,34 @@
+﻿using Heroes.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Repositories
+{
+    public class WeaponRepository<IWeapon> : IRepository<IWeapon>
+    {
+        private List<IWeapon> weapons;
+
+        public WeaponRepository()
+        {
+            this.weapons = new List<IWeapon>();
+        }
+
+        public IReadOnlyCollection<IWeapon> Models => this.weapons.AsReadOnly();
+
+        public void Add(IWeapon model)
+        {
+            this.weapons.Add(model);
+        }
+
+        public IWeapon FindByName(string name)
+        {
+            return (IWeapon)weapons.GetType().GetProperty(name).GetValue(weapons);
+        }
+
+        public bool Remove(IWeapon model)
+        {
+            return this.weapons.Remove(model);
+        }
+    }
+}
